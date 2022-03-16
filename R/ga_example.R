9b2e2a93039f8ef72cf4aff5ddf30b95fe90a430
username <emailaddr>
2022-03-16 12:23:10 +0000

    ga_example documentation

--- R/ga_example.R
+++ R/ga_example.R
@@ -1,3 +1,11 @@
+#' @title ga_example
+#'
+#' @description This function will allow you to run a number of examples
+#'
+#' @param name the name of the example
+#'
+#' @details available examples: 'euclidean'
+#'
 #' @export
 ga_example <- function(name){
   cleaned_name <- trimws(tolower(name));
